d6254f9a9ac4a3a8c91085354fe317ff3091a092
andreas <andreas@immerda.ch>
2008-07-26 19:24:31 +0000

    test

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,17 +1,17 @@
-# modules/perl-FCGI/manifests/init.pp - manage perl-FCGI stuff
+# modules/perl_FCGI/manifests/init.pp - manage perl_FCGI stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "perl-FCGI": }
+# modules_dir { "perl_FCGI": }
 
-class perl-FCGI {
+class perl_FCGI {
     case $operatingsystem {
-        centos: { include perl-FCGI::centos }
-        default: { include perl-FCGI::base }
+        centos: { include perl_FCGI::centos }
+        default: { include perl_FCGI::base }
     }
 }
 
-class perl-FCGI::base {
+class perl_FCGI::base {
 
     package{'perl-FCGI':
         ensure => installed,
@@ -19,6 +19,6 @@ class perl-FCGI::base {
 
 }
 
-class perl-FCGI::centos inherits perl-FCGI::base {
+class perl_FCGI::centos inherits perl_FCGI::base {
 }
 
