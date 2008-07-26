19764b502e896da2fec588219b16ab442668d889
andreas <andreas@immerda.ch>
2008-07-26 19:10:56 +0000

    install perl-FCGI

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,51 +1,24 @@
-# modules/mod_fcgid/manifests/init.pp - manage mod_fcgid stuff
+# modules/perl-FCGI/manifests/init.pp - manage perl-FCGI stuff
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "mod_fcgid": }
+# modules_dir { "perl-FCGI": }
 
-class mod_fcgid {
+class perl-FCGI {
     case $operatingsystem {
-        centos: { include mod_fcgid::centos }
-        default: { include mod_fcgid::base }
+        centos: { include perl-FCGI::centos }
+        default: { include perl-FCGI::base }
     }
 }
 
-class mod_fcgid::base {
-    $config_dir = '/etc/httpd/'
+class perl-FCGI::base {
 
-    package{'mod_fcgid':
+    package{'perl-FCGI':
         ensure => installed,
     }
 
-    if $selinux {
-        include mod_fcgid::selinux
-    }
-
-    file{"${config_dir}/conf.d/fcgid.conf":
-        source => [ "puppet://$server/files/mod_fcgid/${fqdn}/ssl.conf",
-                    "puppet://$server/files/mod_fcgid/fcgid.conf",
-                    "puppet://$server/mod_fcgid/fcgid.conf"
-        ],
-        owner => root, group => 0, mode => 0644;
-    }
-    file{"${config_dir}/conf.d/php.conf":
-        ensure => absent
-    }
-    file{"${config_dir}/conf.d/perl.conf":
-        ensure => absent
-    }
-
-
-#    service{mod_fcgid:
-#        ensure => running,
-#        enable => true,
-#        #hasstatus => true, #fixme!
-#        require => Package[mod_fcgid],
-#    }
-
 }
 
-class mod_fcgid::centos inherits mod_fcgid::base {
+class perl-FCGI::centos inherits perl-FCGI::base {
 }
 
