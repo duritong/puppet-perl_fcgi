7720c0d8cc1f3f78b6b3647de80eb846e0065897
mh <mh@immerda.ch>
2010-05-09 23:44:04 +0200

    refactoring

---
+++ manifests/base.pp
@@ -0,0 +1,5 @@
+class perl-fcgi::base {
+    package{'perl-FCGI':
+        ensure => installed,
+    }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,23 +2,6 @@
 # Copyright (C) 2007 admin@immerda.ch
 # GPLv3
 
-# modules_dir { "perl-fcgi": }
-
 class perl-fcgi {
-    case $operatingsystem {
-        centos: { include perl-fcgi::centos }
-        default: { include perl-fcgi::base }
-    }
+    include perl-fcgi::base
 }
-
-class perl-fcgi::base {
-
-    package{'perl-FCGI':
-        ensure => installed,
-    }
-
-}
-
-class perl-fcgi::centos inherits perl-fcgi::base {
-}
-
